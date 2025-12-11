d277c8a0ec327aafd76870436b4928c7e75b38f0
Shizuka <shizuyas04@gmail.com>
2025-12-11 02:55:56 +0100

    Improved but still flawed
    I really need to master parsing...
    (my hunch is if I managed to convert my list items into integers before starting, this would be much easier... ESPECIALLY test_if_fresh

--- Day 5 Part 1.py
+++ Day 5 Part 1.py
@@ -3,20 +3,9 @@ lines:str=file.readlines()
 fresh=[]
 cut= lines.index("\n")
 fresh_counter=0
-not_rotten=0
-
-#banks = list(map(lambda line: [int(c) for c in line], lines))
 
 list_of_fresh= lines[:cut-1]
 test_if_fresh= lines[cut+1:]
-
-def converting(list):
-     lambda list: [i.rstrip() for i in list]
-     lambda list: [int(i) for i in list]
-
-converting(list_of_fresh)
-converting(test_if_fresh)
-
 
 for r in list_of_fresh:
         indicator= r.index("-")
@@ -25,9 +14,16 @@ for r in list_of_fresh:
         ranges=(fresh_counter_start, fresh_counter_end)
         fresh.append(ranges)
 
-for ranges in fresh:
-       for i in range(len(test_if_fresh)):
-            test=int(test_if_fresh[i])
+
+for i in range(len(test_if_fresh)):
+    for ranges in fresh:
+        test=int(test_if_fresh[i])
+
+        if int(ranges[0])<=test and test<=int(ranges[1]):
+            fresh_counter+=1
+            test_if_fresh.remove(test)
+print(fresh_counter)
+
 
             if int(ranges[0])<=test and test<=int(ranges[1]):
                   fresh_counter+=1
