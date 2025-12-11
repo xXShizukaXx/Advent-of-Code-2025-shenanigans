23e814578569eda1644bcefd04d40ca1553e1afe
Shizuka <shizuyas04@gmail.com>
2025-12-11 02:15:23 +0100

    Update Day 5 Part 1.py because turns out I have been overwhelming my memory lolllllll
    It works on the sample, but not on the input
    also, it seems like I still haven't mastered the art of functions...which is why I had to opt for wrapping again

--- Day 5 Part 1.py
+++ Day 5 Part 1.py
@@ -1,30 +1,35 @@
 file=open("Day 5/D5 test data.txt", "r")
 lines:str=file.readlines()
-fresh=set()
+fresh=[]
 cut= lines.index("\n")
 fresh_counter=0
 not_rotten=0
 
+#banks = list(map(lambda line: [int(c) for c in line], lines))
+
 list_of_fresh= lines[:cut-1]
 test_if_fresh= lines[cut+1:]
+
+def converting(list):
+     lambda list: [i.rstrip() for i in list]
+     lambda list: [int(i) for i in list]
+
+converting(list_of_fresh)
+converting(test_if_fresh)
 
 
 for r in list_of_fresh:
         indicator= r.index("-")
-        fresh_counter = int(r[:indicator])
-        fresh.add(fresh_counter)
-        final=int(r[(indicator+1):])
+        fresh_counter_start = r[:indicator]
+        fresh_counter_end = r[indicator+1:]
+        ranges=(fresh_counter_start, fresh_counter_end)
+        fresh.append(ranges)
 
-        for countup in range (fresh_counter, final):
-            fresh_counter+=1
-            fresh.add(fresh_counter)
+for ranges in fresh:
+       for i in range(len(test_if_fresh)):
+            test=int(test_if_fresh[i])
 
-#print(fresh)    
+            if int(ranges[0])<=test and test<=int(ranges[1]):
+                  fresh_counter+=1
+print(fresh_counter)
 
-for i in range(len(test_if_fresh)):
-
-    tested=int(test_if_fresh[i].rstrip())
-
-    if tested in fresh:
-         not_rotten+=1
-print(not_rotten)
