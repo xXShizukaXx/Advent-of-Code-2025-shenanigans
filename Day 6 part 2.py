d774d297ef5eae964e02960da3db9b50f3e32102
Shizuka <shizuyas04@gmail.com>
2025-12-16 00:08:10 +0100

    yup...still not done :D

--- Day 6 part 2.py
+++ Day 6 part 2.py
@@ -1,26 +1,17 @@
+#deep breath in...deep breath out
+#stupid octopus math...
+#but it's okay...NEVER BACK DOWN; NEVER WHAT??
+#okay...I've literally got everything I need...it's just a hot mess and THAT'S why it doesn't work
 import itertools
-from typing import List
 file=open("Day 6/D6 test data.txt", "r")
 lines= [line.rstrip() for line in file.readlines()]
 sum=0
-
-
-#does my parsing method still suck? Yes. But does it suck less than last week? Absolutely.
-def arkadios_will_be_proud_of_me(l):
-    i = 0
-    for line in lines[:-1]:
-            line= line.split(" ", len(line)) #converts strings into lists by splitting them by spaces
-            line = list(filter(lambda c: c!="", line)) #gets rid of blank spaces
-            line=list(map(lambda line: [str(int(c)) for c in line], line))  #turns every single sequence into its own list. for example, '123' becomes '1','2','3'
-            lines[i]=line   #places new list of lists into the big list TM
-            i+=1
-    line= lines[len(lines)-1] #since last string consists of symbols, its parsing is done separately; but still following the same principle
-    line= line.split(" ", len(line))
-    line = list(filter(lambda c: c!="", line))
-    lines[len(lines)-1]=line #add it to the big list
+#this code....is a HOT mess
+#but that's okay...because it's MY hot mess
+#step...by...step
 
 def transpose[T](arr: list[list[T]]): #shoutout to Arkadios (again) for teaching me this lol
-    for row in arr: assert(len(row))==len(arr[0]) #check if all rows are, indeed, oof the same length
+    for row in arr[:-1]: assert(len(row))==len(arr[0]) #check if all rows are, indeed, oof the same length
 
     out=[]
     for j in range(len(arr[0])):
@@ -28,41 +19,57 @@ def transpose[T](arr: list[list[T]]): #shoutout to Arkadios (again) for teaching
 
     return out
 
-#ok, now how to read numbers vertically? The solution is to zip it!
-def parsing_part_two():
-    b= transpose(lines) #honestly a game changer
+def parsing_1(l): #yes, this used to be arkadios_will_be_proud_of_me() sorry arkadios I need an overview sobbbb
+    i=0
+    for line in l[:-1]:
+        line= line.split(" ", len(line)) #converts strings into lists by splitting them by spaces
+        line = list(filter(lambda c: c!="", line)) #gets rid of blank spaces
+        line=list(map(lambda line: [str(int(c)) for c in line], line))  #turns every single sequence into its own list. for example, '123' becomes '1','2','3'
+        l[i]=line   #places new list of lists into the big list TM
+        i+=1
+    operators= l[(len(l)-1)]
+    l.remove(operators) #remove from the big list
+    operators=operators.split(" ", len(operators)) #since last string consists of symbols, its parsing is done separately; but still following the same principle
+    operators = list(filter(lambda c: c!="", operators))
+    return [l, operators] #ok I got my values...NEXT
+
+def parsing_2(m): #the second part focuses on the actual transposing yada yada
+    b=transpose(m)
     a=[]
+
     for operations in b:
         if (len(operations[1])==len(operations[2])) and len(operations[1])<=len(operations[0]) or (len(operations[0])<=len(operations[1]) and len(operations[1])<len(operations[2])):
-            new = list(itertools.zip_longest(*operations))
+            new = list(itertools.zip_longest(*operations, fillvalue=''))
         else:
-            for values in operations[:-1]:
-                values.reverse() #ofc! flip them!!
-            new = list(itertools.zip_longest(*operations))
+             for values in operations[:-1]:
+                  values.reverse() #ofc! flip them!!
+             new = list(itertools.zip_longest(*operations, fillvalue=''))
         a.append(new)
-        print(a)
-
-        return a
     
-#what a ride... but now, it's time for the actual operation, don't you think? :)
-#okay wait I am about to crash out...parsing_part_two() had been working a few minutes ago and now it doesn't sobbbbb
+    for element in a:
+            i=0
+            for tup in element:
+                my_number=''
+                for j in range (len(tup)): my_number+= tup[j]
+                my_number=int(my_number)
+                element[i]=my_number
+                i+=1
+                #print(element)
+    return a
 
-def calculation(a):
-    number =0
-    value=''
+def calc(numbers, operators):
+    for i in range(len(operators)):
+               temp=1
+               for k in range(len(numbers[i])):
+                    if operators[i]=='+':
+                        sum+=numbers[i][j]
+                    else:
+                         temp*=numbers[i][j]
+                         sum+=temp
+    return sum
+                 
 
-    """for k in a:
-        for n in range(len(k)):
-            if n==0:
-                for m in range(len(k[0])-1):
-                    value+=k[n][m]
-            else:
-                for m in range(len(k[n])):
-                    value+=k[n][m]
-            
-            number= int(value)  """      
-
-
-arkadios_will_be_proud_of_me(lines)                  
-parsing_part_two()
-calculation(parsing_part_two())
+init= parsing_1(lines)[0]
+numbers= parsing_2(init)
+op=parsing_1(lines)[1]
+print(init)
